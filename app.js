9f93f57e66a77f46c5141957b7a797a007072b82
leandrotedini <leandro.tedini@gmail.com>
2022-07-24 19:39:33 -0300

    Part 04 - Exercise 4.1 and 4.2 - Reorganization (module models)

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-
+const Blog = require('./models/blog')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
 
 app.get('/api/blogs', (request, response) => {
   Blog
