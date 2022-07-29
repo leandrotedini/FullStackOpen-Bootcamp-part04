f8e7c36e9447c9ce01a8f33d04651c1a70b704cf
leandrotedini <leandro.tedini@gmail.com>
2022-07-28 22:10:31 -0300

    Part 04 - Exercise 4.5, 4.6 and 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
 
   return 1
@@ -12,8 +14,55 @@ const totalLikes = (blogs = []) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs = []) => {
+
+  if (blogs.length === 0) return null
+
+  const { _id, url, __v, ...favoriteBlog } = blogs.reduce( (previousValue, currentValue) => {
+    return previousValue.likes <  currentValue.likes
+      ? currentValue
+      : previousValue
+  })
+
+  return favoriteBlog
+}
+
+// i dont like at all what i did with this functions
+//but at least i tried to use Lodash library and it works :)
+
+const mostBlogs = (blogs = []) => {
+
+  if (blogs.length === 0) return null
+
+  const result = _.groupBy(blogs, 'author')
+  const resultSorted = _.sortBy(result, result.length)
+  const collectionOfMostComented = _.last(resultSorted)
+  const { author } = collectionOfMostComented[0]
+  const numberOfBlogs = collectionOfMostComented.length
+
+  return { author: author, blogs: numberOfBlogs }
+}
+
+const mostLikes = (blogs = []) => {
+
+  if (blogs.length === 0) return null
+
+  const { author } = favoriteBlog(blogs)
+
+  const likes = _.filter(blogs, { 'author': author })
+    .reduce( (previousValue, currentValue) => {
+      let totalLikes
+      totalLikes = previousValue.likes + currentValue.likes
+      return totalLikes
+    })
+
+  return { author: author, likes: likes }
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
