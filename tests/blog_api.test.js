eb82e6adbb16ef9eafd7ffe68e5e1466c28611aa
leandrotedini <leandro.tedini@gmail.com>
2022-07-29 23:40:29 -0300

    Part 04 - Exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,20 +5,16 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const initialBlogs = [
   {
-    _id: '5a422a851b54a676234d17f7',
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
+    likes: 7
   },
   {
-    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
+    likes: 5
   }
 ]
 
@@ -41,6 +37,11 @@ test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(initialBlogs.length)
 
+})
+
+test('id must be defined', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
